bb69e7b3ff4f64b4b31756fce2c5142ae156f3dc
Romain Magne <romain.magne@edhec.com>
2025-05-30 02:50:34 +0200

    client skeleton

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+//! The gRPC server.
+//!
+// Do not modify this file.
+pub mod client;
+
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use client::example;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Example {
+        #[clap(value_parser)]
+        input: u32,
+    },
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Example { input } => {
+            let output = example(input).await?;
+            println!("{}", output);
+        }
+    }
+
+    Ok(())
 }
---
+++ core/src/rpc/mod.rs
@@ -0,0 +1,3 @@
+//! Automatically generated utilities for RPCs.
+pub mod client_to_node;
+pub mod p2p;
