a9b622f4456e26f1f149d06e1ede0ee6ca0181f9
Romain Magne <romain.magne@edhec.com>
2025-08-02 21:30:21 +0200

    feat: launch boot node and bootstrap another node

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,24 +1,48 @@
 mod client_to_node_server;
-mod p2p;
+mod p2p_node;
 use blockchain_core::log;
 use clap::Parser;
+use dotenv::dotenv;
+use libp2p::PeerId;
 use tokio::task;
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-pub struct Args {
-    #[clap(short, long)]
+#[command(name = "libp2p Kademlia DHT")]
+struct Args {
+    #[arg(long)]
     secondary: bool,
+    #[arg(long)]
+    bootstrap: bool,
+    #[command(subcommand)]
+    kademilia_op: Option<KademiliaOp>,
+}
+
+#[derive(Debug, Parser, Clone)]
+
+enum KademiliaOp {
+    GetPeers {
+        #[arg(long)]
+        peer_id: Option<PeerId>,
+    },
+    PutPkRecord {},
 }
 
 use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    dotenv().ok();
     log::init_logger();
     let args = Args::parse();
     let secondary = args.secondary;
-    let p2p_handle = task::spawn(p2p::start());
+    let bootstrap = args.bootstrap;
+    let kademilia_op = args.kademilia_op;
+    let p2p_handle;
+    if bootstrap {
+        p2p_handle = task::spawn(p2p_node::start_bootstrap_node(kademilia_op));
+    } else {
+        p2p_handle = task::spawn(p2p_node::start_node(kademilia_op));
+    }
     let client_to_node_handle;
     if secondary {
         client_to_node_handle = task::spawn(client_to_node_server::start_secondary());
