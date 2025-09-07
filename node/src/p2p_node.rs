2b2a51df3f27110ba964c078dad3d84f3d629bd8
romain-mg <romiffmg@gmail.com>
2025-09-07 11:38:34 -0700

    feat: p2p node

--- node/src/p2p_node.rs
+++ node/src/p2p_node.rs
@@ -1,38 +1,396 @@
-use crate::KademiliaOp;
 use anyhow::{Result, bail};
 use base64::prelude::*;
-use blockchain_core::log;
-use futures::prelude::*;
+use blockchain_core::{self, log};
+use futures::{
+    channel::{mpsc, oneshot},
+    prelude::*,
+    StreamExt,
+};
 use libp2p::{
     PeerId,
     bytes::BufMut,
     identity::{self, Keypair},
-    kad::{self, NoKnownPeers},
-    noise,
-    swarm::{StreamProtocol, SwarmEvent},
+    kad::{store::MemoryStore, self}, noise,
+    swarm::{NetworkBehaviour, StreamProtocol, SwarmEvent, Swarm},
     tcp, yamux,
+    request_response::{self, OutboundRequestId, ProtocolSupport, ResponseChannel},
+    core::Multiaddr,
+    multiaddr::Protocol
 };
 use std::{
     env,
     num::NonZeroUsize,
     ops::Add,
     time::{Duration, Instant},
+    collections::{hash_map, HashMap},
+    error::Error,
 };
 use tracing_subscriber::EnvFilter;
+use serde::{Serialize, Deserialize};
+
 
 const BOOTNODE_ID: &str = "12D3KooWCxCPBaitzgsjvogRgcLEJ3i1CfHk5HhxUF3ykTg7fs2U";
 const BOOTNODE_MULTIADDR: &str =
     "/ip4/192.168.12.37/tcp/4001/p2p/12D3KooWCxCPBaitzgsjvogRgcLEJ3i1CfHk5HhxUF3ykTg7fs2U";
+const PROTO_NAME: StreamProtocol = StreamProtocol::new("/blockchain/1.0.0");
 
-const IPFS_PROTO_NAME: StreamProtocol = StreamProtocol::new("/blockchain/kad/1.0.0");
+/// Creates the network components, namely:
+///
+/// - The network client to interact with the network layer from anywhere within your application.
+///
+/// - The network event stream, e.g. for incoming requests.
+///
+/// - The network task driving the network itself.
+pub(crate) async fn new(
+    secret_key_seed: Option<u8>,
+) -> Result<(Client, impl Stream<Item = Event>, EventLoop), Box<dyn Error>> {
+    // Create a public/private key pair, either random or based on a seed.
+    let id_keys = match secret_key_seed {
+        Some(seed) => {
+            let mut bytes = [0u8; 32];
+            bytes[0] = seed;
+            identity::Keypair::ed25519_from_bytes(bytes).unwrap()
+        }
+        None => identity::Keypair::generate_ed25519(),
+    };
+    let peer_id = id_keys.public().to_peer_id();
 
-pub async fn start_node(kademilia_op: Option<KademiliaOp>) -> Result<()> {
-    log::info!("Starting p2p node");
-    let key_pair = identity::Keypair::generate_ed25519();
-    launch_swarm(key_pair, kademilia_op, false).await
+    let mut swarm = libp2p::SwarmBuilder::with_existing_identity(id_keys)
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,
+            yamux::Config::default,
+        )?
+        .with_behaviour(|key| Behaviour {
+            kademlia: kad::Behaviour::new(
+                peer_id,
+                kad::store::MemoryStore::new(key.public().to_peer_id()),
+            ),
+            request_response: request_response::cbor::Behaviour::new(
+                [(
+                    StreamProtocol::new("/blockchain/1.0.0"),
+                    ProtocolSupport::Full,
+                )],
+                request_response::Config::default(),
+            ),
+        })?
+        .with_swarm_config(|c| c.with_idle_connection_timeout(Duration::from_secs(60)))
+        .build();
+
+    swarm
+        .behaviour_mut()
+        .kademlia
+        .set_mode(Some(kad::Mode::Server));
+
+    let (command_sender, command_receiver) = mpsc::channel(0);
+    let (event_sender, event_receiver) = mpsc::channel(0);
+
+    Ok((
+        Client {
+            sender: command_sender,
+        },
+        event_receiver,
+        EventLoop::new(swarm, command_receiver, event_sender),
+    ))
 }
 
-pub async fn start_bootstrap_node(kademilia_op: Option<KademiliaOp>) -> Result<()> {
+#[derive(Clone)]
+pub(crate) struct Client {
+    sender: mpsc::Sender<Command>,
+}
+
+impl Client {
+    /// Listen for incoming connections on the given address.
+    pub(crate) async fn start_listening(
+        &mut self,
+        addr: Multiaddr,
+    ) -> Result<(), Box<dyn Error + Send>> {
+        let (sender, receiver) = oneshot::channel();
+        self.sender
+            .send(Command::StartListening { addr, sender })
+            .await
+            .expect("Command receiver not to be dropped.");
+        receiver.await.expect("Sender not to be dropped.")
+    }
+
+    /// Dial the given peer at the given address.
+    pub(crate) async fn dial(
+        &mut self,
+        peer_id: PeerId,
+        peer_addr: Multiaddr,
+    ) -> Result<(), Box<dyn Error + Send>> {
+        let (sender, receiver) = oneshot::channel();
+        self.sender
+            .send(Command::Dial {
+                peer_id,
+                peer_addr,
+                sender,
+            })
+            .await
+            .expect("Command receiver not to be dropped.");
+        receiver.await.expect("Sender not to be dropped.")
+    }
+
+    pub(crate) async fn request_blockchain_sync(
+        &mut self,
+        peer: PeerId,
+    ) -> Result<Vec<u8>, Box<dyn Error + Send>> {
+        let (sender, receiver) = oneshot::channel();
+        self.sender
+            .send(Command::RequestBlockchainSync {
+                peer,
+                sender,
+            })
+            .await
+            .expect("Command receiver not to be dropped.");
+        receiver.await.expect("Sender not be dropped.")
+    }
+
+    pub(crate) async fn respond_blockchain_sync(
+        &mut self,
+        serialized_blockchain: Vec<u8>,
+        channel: ResponseChannel<BlockchainSyncResponse>,
+    ) {
+        self.sender
+            .send(Command::RespondBlockchainSync { serialized_blockchain, channel })
+            .await
+            .expect("Command receiver not to be dropped.");
+    }
+}
+
+
+pub(crate) struct EventLoop {
+    swarm: Swarm<Behaviour>,
+    command_receiver: mpsc::Receiver<Command>,
+    event_sender: mpsc::Sender<Event>,
+    pending_dial: HashMap<PeerId, oneshot::Sender<Result<(), Box<dyn Error + Send>>>>,
+    pending_request_blockchain_sync:
+        HashMap<OutboundRequestId, oneshot::Sender<Result<Vec<u8>, Box<dyn Error + Send>>>>,
+}
+
+impl EventLoop {
+    fn new(
+        swarm: Swarm<Behaviour>,
+        command_receiver: mpsc::Receiver<Command>,
+        event_sender: mpsc::Sender<Event>,
+    ) -> Self {
+        Self {
+            swarm,
+            command_receiver,
+            event_sender,
+            pending_dial: Default::default(),
+            pending_request_blockchain_sync: Default::default(),
+        }
+    }
+
+    pub(crate) async fn run(mut self) {
+        loop {
+            tokio::select! {
+                event = self.swarm.select_next_some() => self.handle_event(event).await,
+                command = self.command_receiver.next() => match command {
+                    Some(c) => self.handle_command(c).await,
+                    // Command channel closed, thus shutting down the network event loop.
+                    None=>  return,
+                },
+            }
+        }
+    }
+
+    async fn handle_event(&mut self, event: SwarmEvent<BehaviourEvent>) {
+        match event {
+            SwarmEvent::Behaviour(BehaviourEvent::Kademlia(_)) => {}
+            SwarmEvent::Behaviour(BehaviourEvent::RequestResponse(
+                request_response::Event::Message { message, .. },
+            )) => match message {
+                request_response::Message::Request {
+               channel, ..
+                } => {
+                    self.event_sender
+                        .send(Event::InboundRequest {
+                            request: String::from("blockchain sync request"),
+                            channel,
+                        })
+                        .await
+                        .expect("Event receiver not to be dropped.");
+                }
+                request_response::Message::Response {
+                    request_id,
+                    response,
+                } => {
+                    let _ = self
+                        .pending_request_blockchain_sync
+                        .remove(&request_id)
+                        .expect("Request to still be pending.")
+                        .send(Ok(response.0));
+                }
+            },
+            SwarmEvent::Behaviour(BehaviourEvent::RequestResponse(
+                request_response::Event::OutboundFailure {
+                    request_id, error, ..
+                },
+            )) => {
+                let _ = self
+                    .pending_request_blockchain_sync
+                    .remove(&request_id)
+                    .expect("Request to still be pending.")
+                    .send(Err(Box::new(error)));
+            }
+            SwarmEvent::Behaviour(BehaviourEvent::RequestResponse(
+                request_response::Event::ResponseSent { .. },
+            )) => {}
+            SwarmEvent::NewListenAddr { address, .. } => {
+                let local_peer_id = *self.swarm.local_peer_id();
+                log::info!(
+                    "Local node is listening on {:?}",
+                    address.with(Protocol::P2p(local_peer_id))
+                );
+            }
+            SwarmEvent::IncomingConnection { .. } => {}
+            SwarmEvent::ConnectionEstablished {
+                peer_id, endpoint, ..
+            } => {
+                if endpoint.is_dialer() {
+                    if let Some(sender) = self.pending_dial.remove(&peer_id) {
+                        let _ = sender.send(Ok(()));
+                    }
+                }
+            }
+            SwarmEvent::ConnectionClosed { .. } => {}
+            SwarmEvent::OutgoingConnectionError { peer_id, error, .. } => {
+                if let Some(peer_id) = peer_id {
+                    if let Some(sender) = self.pending_dial.remove(&peer_id) {
+                        let _ = sender.send(Err(Box::new(error)));
+                    }
+                }
+            }
+            SwarmEvent::IncomingConnectionError { .. } => {}
+            SwarmEvent::Dialing {
+                peer_id: Some(peer_id),
+                ..
+            } => eprintln!("Dialing {peer_id}"),
+            e => panic!("{e:?}"),
+        }
+    }
+
+    async fn handle_command(&mut self, command: Command) {
+        match command {
+            Command::StartListening { addr, sender } => {
+                let _ = match self.swarm.listen_on(addr) {
+                    Ok(_) => sender.send(Ok(())),
+                    Err(e) => sender.send(Err(Box::new(e))),
+                };
+            }
+            Command::Dial {
+                peer_id,
+                peer_addr,
+                sender,
+            } => {
+                if let hash_map::Entry::Vacant(e) = self.pending_dial.entry(peer_id) {
+                    self.swarm
+                        .behaviour_mut()
+                        .kademlia
+                        .add_address(&peer_id, peer_addr.clone());
+                    match self.swarm.dial(peer_addr.with(Protocol::P2p(peer_id))) {
+                        Ok(()) => {
+                            e.insert(sender);
+                        }
+                        Err(e) => {
+                            let _ = sender.send(Err(Box::new(e)));
+                        }
+                    }
+                } else {
+                    todo!("Already dialing peer.");
+                }
+            }
+            Command::RequestBlockchainSync {
+                peer,
+                sender,
+            } => {
+                let request_id = self
+                    .swarm
+                    .behaviour_mut()
+                    .request_response
+                    .send_request(&peer, BlockchainSyncRequest());
+                self.pending_request_blockchain_sync.insert(request_id, sender);
+            }
+            Command::RespondBlockchainSync { serialized_blockchain, channel } => {
+                self.swarm
+                    .behaviour_mut()
+                    .request_response // SAME
+                    .send_response(channel, BlockchainSyncResponse(serialized_blockchain))
+                    .expect("Connection to peer to be still open.");
+            }
+        }
+    }
+}
+
+
+#[derive(Debug)]
+enum Command {
+    StartListening {
+        addr: Multiaddr,
+        sender: oneshot::Sender<Result<(), Box<dyn Error + Send>>>,
+    },
+    Dial {
+        peer_id: PeerId,
+        peer_addr: Multiaddr,
+        sender: oneshot::Sender<Result<(), Box<dyn Error + Send>>>,
+    },
+    RequestBlockchainSync {
+        peer: PeerId,
+        sender: oneshot::Sender<Result<Vec<u8>, Box<dyn Error + Send>>>,
+    },
+    RespondBlockchainSync {
+        serialized_blockchain: Vec<u8>,
+        channel: ResponseChannel<BlockchainSyncResponse>,
+    },
+}
+
+#[derive(NetworkBehaviour)]
+struct Behaviour {
+    request_response: request_response::cbor::Behaviour<BlockchainSyncRequest, BlockchainSyncResponse>,
+    kademlia: kad::Behaviour<kad::store::MemoryStore>,
+}
+
+#[derive(Debug)]
+pub(crate) enum Event {
+    InboundRequest {
+        request: String,
+        channel: ResponseChannel<BlockchainSyncResponse>,
+    },
+}
+
+// Simple blockchain sync protocol
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+struct BlockchainSyncRequest();
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub(crate) struct BlockchainSyncResponse(Vec<u8>);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pub async fn start_node() -> Result<()> {
+    log::info!("Starting p2p node");
+    let key_pair = identity::Keypair::generate_ed25519();
+    launch_swarm(key_pair, false).await
+}
+
+pub async fn start_bootstrap_node() -> Result<()> {
     log::info!("Starting bootstrap p2p node");
     let _ = tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
@@ -47,12 +405,11 @@ pub async fn start_bootstrap_node(kademilia_op: Option<KademiliaOp>) -> Result<(
     if &bootnode_id != BOOTNODE_ID {
         panic!("Wrong bootnode ID");
     }
-    launch_swarm(decoded_keys, kademilia_op, true).await
+    launch_swarm(decoded_keys, true).await
 }
 
 async fn launch_swarm(
     key_pair: Keypair,
-    kademilia_op: Option<KademiliaOp>,
     bootstrap: bool,
 ) -> Result<()> {
     let mut swarm = libp2p::SwarmBuilder::with_existing_identity(key_pair.clone())
@@ -65,7 +422,7 @@ async fn launch_swarm(
         .with_dns()?
         .with_behaviour(|key_pair| {
             // Create a Kademlia behaviour.
-            let mut cfg = kad::Config::new(IPFS_PROTO_NAME);
+            let mut cfg = kad::Config::new(PROTO_NAME);
             cfg.set_query_timeout(Duration::from_secs(5 * 60));
             let store = kad::store::MemoryStore::new(key_pair.public().to_peer_id());
             kad::Behaviour::with_config(key_pair.public().to_peer_id(), store, cfg)
@@ -96,29 +453,6 @@ async fn launch_swarm(
 
     swarm.behaviour_mut().set_mode(Some(kad::Mode::Server));
 
-    match kademilia_op {
-        Some(KademiliaOp::GetPeers { peer_id }) => {
-            let peer_id = peer_id.unwrap_or(PeerId::random());
-            log::info!("Searching for the closest peers to {peer_id}");
-            swarm.behaviour_mut().get_closest_peers(peer_id);
-        }
-        Some(KademiliaOp::PutPkRecord {}) => {
-            log::info!("Putting PK record into the DHT");
-            let mut pk_record_key = vec![];
-            pk_record_key.put_slice("/pk/".as_bytes());
-            pk_record_key.put_slice(swarm.local_peer_id().to_bytes().as_slice());
-
-            let mut pk_record =
-                kad::Record::new(pk_record_key, key_pair.public().encode_protobuf());
-            pk_record.publisher = Some(*swarm.local_peer_id());
-            pk_record.expires = Some(Instant::now().add(Duration::from_secs(60)));
-
-            swarm
-                .behaviour_mut()
-                .put_record(pk_record, kad::Quorum::N(NonZeroUsize::new(3).unwrap()))?;
-        }
-        None => {}
-    }
     loop {
         let event = swarm.select_next_some().await;
         match event {
@@ -126,8 +460,6 @@ async fn launch_swarm(
                 result: kad::QueryResult::GetClosestPeers(Ok(ok)),
                 ..
             }) => {
-                // The example is considered failed as there
-                // should always be at least 1 reachable peer.
                 if ok.peers.is_empty() {
                     bail!("Query finished with no closest peers.")
                 }
